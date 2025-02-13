d53ea640d74e3f80d483864e479869d8f8c10174
danielrsenna <danielrsenna@gmail.com>
2025-02-13 16:23:29 -0300

    dash

--- noh_faq_search/pages/dash_state.py
+++ noh_faq_search/pages/dash_state.py
@@ -11,10 +11,10 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class DashboardState(rx.State):
-    search_logs: list[dict] = []  # Dados brutos de busca
+    search_logs: list[dict] = []  
     total_searches: int = 0
-    top_articles_df: pd.DataFrame = pd.DataFrame()  # DataFrame para top artigos
-    searches_by_hour_df: list[dict] = []  # DataFrame para pesquisas por hora
+    top_articles_df: list[dict] = []  
+    searches_by_hour_df: list[dict] = []  
     is_loading: bool = False
 
     def load_all_data(self):
@@ -27,7 +27,8 @@ class DashboardState(rx.State):
             if response.data:
                 self.search_logs = response.data
                 self.total_searches = len(self.search_logs)
-                self.gen_searches_by_hour_df()
+                self.gen_searches_by_hour()
+                self.generate_top_articles()
                 logging.info(f"Dados carregados: {self.total_searches} registros.")
             else:
                 self.search_logs = []
@@ -38,10 +39,22 @@ class DashboardState(rx.State):
         finally:
             self.is_loading = False
 
-    def gen_searches_by_hour_df(self):
+    def gen_searches_by_hour(self):
         df = pd.DataFrame(self.search_logs)
         df["search_at"] = pd.to_datetime(df["search_at"])
         searches_by_hour = df.groupby(df["search_at"].dt.strftime("%Y-%m-%d %H:00")).size().reset_index(name="pesquisas")
         searches_by_hour.rename(columns={"search_at": "hora"}, inplace=True)
         self.searches_by_hour_df = searches_by_hour.to_dict('records')
+    
+    def generate_top_articles(self):
+        df = pd.DataFrame(self.search_logs)
+        articles_df = pd.json_normalize(df["embeddings_results"]).explode("titulo_artigo")
+        top_articles = (
+            articles_df["titulo_artigo"]
+            .value_counts()
+            .reset_index(name="Número de Ocorrências")
+            .rename(columns={"index": "Título do Artigo"})
+            .head(10)
+        )
+        self.top_articles_df = top_articles.to_dict('records')
         
